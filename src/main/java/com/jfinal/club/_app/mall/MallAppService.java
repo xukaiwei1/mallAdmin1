8e771542f104802d59ceb30a63491f913f75290f
wangli05 <wangli05@renrenche.com>
2019-02-18 11:00:17 +0800

    获取商品的类型

--- src/main/java/com/jfinal/club/_app/mall/MallAppService.java
+++ src/main/java/com/jfinal/club/_app/mall/MallAppService.java
@@ -1,12 +1,12 @@
 /**
  * 请勿将俱乐部专享资源复制给其他人，保护知识产权即是保护我们所在的行业，进而保护我们自己的利益
  * 即便是公司的同事，也请尊重 JFinal 作者的努力与付出，不要复制给同事
- * 
+ *
  * 如果你尚未加入俱乐部，请立即删除该项目，或者现在加入俱乐部：http://jfinal.com/club
- * 
+ *
  * 俱乐部将提供 jfinal-club 项目文档与设计资源、专用 QQ 群，以及作者在俱乐部定期的分享与答疑，
  * 价值远比仅仅拥有 jfinal club 项目源代码要大得多
- * 
+ *
  * JFinal 俱乐部是五年以来首次寻求外部资源的尝试，以便于有资源创建更加
  * 高品质的产品与服务，为大家带来更大的价值，所以请大家多多支持，不要将
  * 首次的尝试扼杀在了摇篮之中
@@ -34,13 +34,12 @@ public class MallAppService {
 
 	private MlMall dao = new MlMall().dao();
 	private MlResource resourceDao = new MlResource().dao();
+	private MlGoodsType goodsTypeDao = new MlGoodsType().dao();
 
 	private   static final  String COLUMNS="id,mall_name,mall_desc,mall_code,mall_type,status";
 	private   static final  String RESOURCE_COLUMNS="id,resource_name,resource_type,resource_value,status,orders";
+	private   static final  String MLGOODSTYPE_COLUMNS="id,type_name,type_code,parent_type,status,current_mall_id";
 	private   static final  int RESOURCE_TYPE=1;
-
-
-
 
 	public Ret getMallconfig(int mlCode) {
 		Kv para = Kv.by("columns", COLUMNS).set("mlCode", mlCode);
@@ -54,6 +53,13 @@ public class MallAppService {
 		SqlPara sqlPara = resourceDao.getSqlPara("mall.getMallBanner", para);
 		List<MlResource> mlResourcess = resourceDao.find(sqlPara);
 		return Ret.ok("mlBanners", mlResourcess);
+	}
+
+	public Ret getGoodsType(int mallId) {
+		Kv para = Kv.by("columns", MLGOODSTYPE_COLUMNS).set("mallId", mallId);
+		SqlPara sqlPara = resourceDao.getSqlPara("mall.getGoodsType", para);
+		List<MlGoodsType> mlGoodsTypes = goodsTypeDao.find(sqlPara);
+		return Ret.ok("mlGoodsTypes", mlGoodsTypes);
 	}
 
 
